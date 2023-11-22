58b69c52a361e638674fee294e3adefe3c84fb2a
huanghanzhilian <1319639755@qq.com>
2023-11-22 11:23:26 +0800

    feat: observe UIView

--- first-coding/ViewController.m
+++ first-coding/ViewController.m
@@ -7,6 +7,42 @@
 
 #import "ViewController.h"
 
+/*
+ 为了观察UIView 从创建到被展示的流程
+ 集成UIView，创建一个TestView，它继承于UIView
+ */
+
+@interface TestView : UIView
+@end
+
+@implementation TestView
+
+- (instancetype) init{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+    
+}
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow:newWindow];
+}
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+    
+}
+
+@end
+
 @interface ViewController ()
 
 @end
@@ -16,12 +52,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIView *view = [[UIView alloc] init];
-    view.backgroundColor = [UIColor redColor];
-    view.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:view];
+    self.view.backgroundColor = [UIColor redColor];
+//    UIView *view = [[UIView alloc] init];
+//    view.backgroundColor = [UIColor redColor];
+//    view.frame = CGRectMake(100, 100, 100, 100);
+//    [self.view addSubview:view];
     
-    UIView *view2 = [[UIView alloc] init];
+    TestView *view2 = [[TestView alloc] init];
     view2.backgroundColor = [UIColor greenColor];
     view2.frame = CGRectMake(150, 150, 100, 100);
     [self.view addSubview:view2];
